7bcd311b21f4324aa1458c7b4e058157e27ee30e
ItsIvan <107851166+ivanChri@users.noreply.github.com>
2023-09-20 05:35:38 +0700

    Update storage.ts

--- src/app/utils/storage/storage.ts
+++ src/app/utils/storage/storage.ts
@@ -1,12 +1,18 @@
-'use client'
+import { MenuInterface } from "../interface/menu.interface"
+import { OrderInterface } from "../interface/order.interface"
 export function loadData (key:string) {
-  const savedData = localStorage.getItem(key)
+  let savedData
+  if (typeof window !== 'undefined') {
+   savedData = localStorage.getItem(key)
+  }
   if(savedData){
     return JSON.parse(savedData)
   }else{
     return []
  }
 }
-export function addData (key:string,data:any) {
-  return localStorage.setItem(key,JSON.stringify(data))
+export function addData (key:string,data:MenuInterface[] | OrderInterface[]) {
+  if (typeof window !== 'undefined') {
+    return localStorage.setItem(key,JSON.stringify(data))
+  }
 }
